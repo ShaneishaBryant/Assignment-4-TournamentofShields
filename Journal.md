69453f3f9d48ddb36f6bfc8cd17961df138dfbe6
ShaneishaBryant <shaneisharichardson@student.bpcc.edu>
2026-09-07 17:42:48 -0500

    Phase 1 Complete
    Steps I created to complete phase 1:
    > created class
    > created instance variable
    >instantiated multiple variables
    >printed out values

--- Journal.md
+++ Journal.md
@@ -1,2 +1,6 @@
 # Journal
-Write your Journal questions and notes here.
+Describe a scenario in a larger game where allowing outside code to directly change a Guardian’s defenseRating would cause severe bugs or cheating. How does direct field access limit your control as a developer?
+
+In playing a multiplayer game where anyone can just reach into the code and directly change guardian.defenseRating. That opens the door to huge bugs and easy cheating. A player could max out their defense or push it way past the game's limits, totally breaking game balance. Worse yet, throwing crazy numbers into that field could mess up damage math so badly that it crashes the entire game server.
+
+Direct field access prevents developers from enforcing input constraints and leads to fragile code. If you later change or rename an instance variable, you are forced to manually update every reference across the entire codebase.
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
-    static void main() {
-        System.out.println("Hello World!");
+    static void main(String[] args){
+
+        //instantiating multiple guardians
+        Guardian guardian1 = new Guardian();
+        guardian1.name = "Pegasus";
+        guardian1.defenseRating = 85;
+
+        Guardian guardian2 = new Guardian();
+        guardian2.name = "Orion";
+        guardian2.defenseRating = 95;
+
+        Guardian guardian3 = new Guardian();
+        guardian3.name = "Mordecai";
+        guardian3.defenseRating = 50;
+
+        Guardian guardian4 = new Guardian();
+        guardian4.name = "Rigby";
+        guardian4.defenseRating = 25;
+
+        System.out.println("Guardian 1: " + guardian1.name + " | Defense Rating: " + guardian1.defenseRating);
+        System.out.println("Guardian 2: " + guardian2.name + " | Defense Rating: " + guardian2.defenseRating);
+        System.out.println("Guardian 3: " + guardian3.name + " | Defense Rating: " + guardian3.defenseRating);
+        System.out.println("Guardian 4: " + guardian4.name + " | Defense Rating: " + guardian4.defenseRating);
+
+        System.out.println();
+
+        //directly modify fields
+        guardian4.defenseRating += 10;
+        System.out.println("Guardian 4 --> New Defense Rating: " + guardian4.defenseRating);
+
+
+
+
     }
 }
